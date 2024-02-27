31b00ca99a9dc1426f6b600877b21576007b6a3a
Necroneco <caibinqing@live.com>
2024-02-27 10:36:45 +0800

    整理门锁消息，自用

--- custom_components/aqara_gateway/core/lock_data.py
+++ custom_components/aqara_gateway/core/lock_data.py
@@ -38,70 +38,109 @@ DEVICE_MAPPINGS = {
 LOCK_NOTIFICATION = {
     "latch_state": {
         "default": "Latch state changed",
-        "0": "Remove the locking from inside",
-        "1": "Reverse locked"},
+        "0": "解除反锁",  # "Remove the locking from inside",
+        "1": "开启反锁",  # "Reverse locked",
+    },
     "lock": {
         "default": "Lock state changed",
-        "0": "Door is open",
-        "1": "Door is closed",
-        "2": "Door is not close",
-        "3": "Doorbell is ringing",
-        "4": "Lock is damaged",
-        "5": "Door is conceal",
+        "0": "门已开",  # "Door is open",
+        "1": "门已关",  # "Door is closed",
+        "2": "门未关",  # "Door is not close",
+        "3": "有人按门铃",  # "Doorbell is ringing",
+        "4": "门锁被破坏",  # "Lock is damaged",
+        "5": "门虚掩",  # "Door is conceal",
         "6": "Other 1",
         "7": "Other 2"},
-    "door": {
-        "default": "door state changed",
-        "0": "Unknown",
-        "1": "The door cannot be locked",
-        "2": "The door is not closed",
-        "3": "The door is not locked",
-        "4": "The door is locked",
-        "5": "The door is auti-locked",
-        "6": "The door is unlocked",
-        "7": "The door is locked and auti-locked",
-        "8": "The door is left unlocked"},
+    # "door": {
+    #     "default": "door state changed",
+    #     "0": "Unknown",
+    #     "1": "无法上锁",  # "The door cannot be locked",
+    #     "2": "已开门",  # "The door is not closed", 0---
+    #     "3": "未上锁",  # "The door is not locked", 1100
+    #     "4": "已上锁",  # "The door is locked", 1110
+    #     "5": "已反锁",  # "The door is auti-locked", ++01
+    #     "6": "已开锁",  # "The door is unlocked", 10??
+    #     "7": "已上锁且反锁",  # "The door is locked and auti-locked", ++11
+    #     "8": "仍然开锁",  # "The door is left unlocked" +---
+    # },
     "lock_event": {
         "default": "Got lock event",
         "0": "Unlock",
         "1": "Lock"
     },
-    "unlock from inside": {"default": "Unlock from Inside"},
+    "unlock from inside": {
+        "default": "Unlock from Inside",
+        "0": "室内开锁",
+    },
     "someone detected": {"default": "Someone is lingering at the door"},
     "li battery notify":
         {"default": "Li Battery notify",
             "0": "Li Battery is abnormal",
             "1": "Li Battery is normal"},
-    "battery notify":
-        {"default": "Battery notify",
-            "0": "Battery is die",
-            "1": "Battery level is low",
-            "2": "Battery level is middle",
-            "3": "Battery level is full"},
+    # "battery notify":
+    #     {"default": "Battery notify",
+    #         "0": "Battery is die",
+    #         "1": "Battery level is low",
+    #         "2": "Battery level is middle",
+    #         "3": "Battery level is full"},
     "camera connected": {"default": "Camera is connected"},
     "open in away mode": {
         "default":
             "In the Away-from-home Mode, someone opens the door indoors"},
-    "lock by handle": {"default": "Lock by door handle"},
-    "auto locking":
-        {"default": "Auto locking state changed",
-         "1": "Auto locking"},
-    "unlock by password": {"default": "Unlocked with Keypad by user"},
-    "unlock by fingerprint": {"default": "Unlocked with Fingerprint by user"},
-    "unlock by bluetooth": {"default": "Unlocked with Bluetooth by user"},
-    "unlock by homekit": {"default": "Unlocked with HomeKit by user"},
-    "unlock by key": {"default": "Unlocked with key by user"},
-    "unlock by nfc": {"default": "Unlocked with NFC by user"},
-    "unlock by face": {"default": "Unlocked with Face by user"},
-    "unlock by temporary password": {"default": "Unlocked with Temporary Password"},
+    "lock by handle": {
+        "default": "Lock by door handle",
+        "0": "解除方锁",
+        "1": "上提把手锁门",
+    },
+    "auto locking": {
+        "default": "自动上锁状态改变",
+        "1": "自动上锁",
+    },
+    "unlock by password": {
+        "default": "密码开锁",  # "Unlocked with Keypad by user"
+    },
+    "unlock by fingerprint": {
+        "default": "指纹开锁",  # "Unlocked with Fingerprint by user"
+    },
+    "unlock by bluetooth": {
+        "default": "蓝牙开锁",  # "Unlocked with Bluetooth by user"
+    },
+    "unlock by homekit": {
+        "default": "Unlocked with HomeKit by user",
+        "0": "Aqara Home 蓝牙开锁",
+        "2": "HomeKit开锁",
+    },
+    "unlock by key": {
+        "default": "钥匙或应急旋钮开锁",  # "Unlocked with key by user"
+    },
+    "unlock by nfc": {
+        "default": "NFC开锁",  # "Unlocked with NFC by user"
+    },
+    "unlock by face": {
+        "default": "人脸开锁",  # "Unlocked with Face by user"
+    },
+    "unlock by temporary password": {
+        "default": "一次性密码开锁",  # "Unlocked with Temporary Password"
+    },
+    # "key_type": {
+    #     "default": "正常的开锁验证通过",
+    #     "0": "指纹开锁",
+    #     "1": "密码开锁",
+    #     "3": "蓝牙开锁",
+    #     "4": "一次性密码开锁",
+    #     "5": "钥匙或应急旋钮开锁",
+    #     "8": "HomeKit开锁",
+    #     "9": "人脸开锁",
+    # },
     "away mode": {
         "default": "Away mode changed",
-        "0": "Away-from-home mode is removed",
-        "1": "Away-from-home mode is enabled"},
+        "0": "离家模式已解除",  # "Away-from-home mode is removed",
+        "1": "离家模式已开启",  # "Away-from-home mode is enabled",
+    },
     "nfc added": {"default": "Added NFC card or Tag"},
     "nfc removed": {"default": "Removed NFC card or Tag"},
     "verification failed": {
-        "default": "door lock verifications failed",
+        "default": "门锁验证失败",  # "door lock verifications failed",
         "3235774464": "Frequent door opening failures due to incorrect passwords",
         "3235774465": "Frequent door opening failures due to incorrect fingerprints",
         "3235774469": "Frequent door openings with abnormal keys",
